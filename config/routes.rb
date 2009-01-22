ebe8998622fcde6a4efe6286daa20fe566425114
Jim Pick <jim@jimpick.com>
2009-01-22 11:51:33 -0800

    Add authenticated and bagpipes.
    Untested.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,18 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :topics, :has_many => [:messages]
   map.new_reply 'topics/:topic_id/parent/:parent_id', :controller => 'messages', :action => 'new'
 
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
+  map.resources :members
+  map.resources :topics, :has_many => [:messages]
+  map.new_reply 'topics/:topic_id/parent/:parent_id', :controller => 'messages', :action => 'new'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
